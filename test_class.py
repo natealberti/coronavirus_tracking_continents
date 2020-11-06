d2dbad2ef4ec6377a597930f501db20cc925348b
Nate <65456795+natealberti@users.noreply.github.com>
2020-11-05 21:27:23 -0700

    added daily report

--- test_class.py
+++ test_class.py
@@ -1,41 +1,19 @@
-import region
+from coronavirus_tracking import region
+import email_text_alerts.alert
 import pandas as pd
 import datetime
 import time
 
 def main():
-    # initial list of region objects
-    continents = initialize_regions()
+    # continents dataframe #
+    continents = initialize_continents()
+    #print_continents(continents)
 
-    ### pandas series ###
+    # top 10 countries dataframe #
+    countries = initialize_top10_countries()
+    #print_countries(countries)
 
-    # region names
-    s = [continents[i].get_region_name() for i in range (len(continents))]
-    names = pd.Series(s)
-
-    # total cases
-    s = [continents[i].get_new_cases() for i in range(len(continents))]
-    total_cases = pd.Series(s).astype('int32')
-
-    # total deaths
-    s = [continents[i].get_new_deaths() for i in range(len(continents))]
-    total_deaths = pd.Series(s).astype('int32')
-
-    # mortality rate
-    s = [str(continents[i].get_mortality_rate()) + '%' for i in range(len(continents))]
-    mortality_rate = pd.Series(s).astype('str')
-
-
-    # constructing the dataframe
-    data = {
-        'continent' : names,
-        'new cases' : total_cases,
-        'new deaths' : total_deaths,
-        'mortality rate': mortality_rate,
-    }
-    df = pd.DataFrame(data, columns=['continent', 'new cases', 'new deaths', 'mortality rate'])
-    print(df)
-    print('\nFROM: ' + str(datetime.date.today()))
+    daily_report()
 
 ### functions ###
 
@@ -56,10 +34,30 @@ def get_region_key(name):
         key = 6
     elif (name == 'global'):
         key = 8
+    if (name == 'usa'):
+        key = 9
+    elif (name == 'india'):
+        key = 10
+    elif (name == 'brazil'):
+        key = 11
+    elif (name == 'russia'):
+        key = 12
+    elif (name == 'spain'):
+        key = 13
+    elif (name == 'colombia'):
+        key = 14
+    elif (name == 'argentina'):
+        key = 15
+    elif (name == 'peru'):
+        key = 16
+    elif (name == 'mexico'):
+        key = 17
+    elif (name == 'france'):
+        key = 18
     return key
 
-# initializes the regions and their keys
-def initialize_regions():
+# initializes the continents and their keys
+def initialize_continents():
     # region objects connected to their keys
     north_america = region.region(get_region_key('north america'))
     south_america = region.region(get_region_key('south america'))
@@ -79,6 +77,139 @@ def initialize_regions():
         globe]
 
     return continents
+
+# initialize the 10 countries with the most cases and their keys
+def initialize_top10_countries():
+    # region objects connected to their keys
+    usa = region.region(get_region_key('usa'))
+    india = region.region(get_region_key('india'))
+    brazil = region.region(get_region_key('brazil'))
+    russia = region.region(get_region_key('russia'))
+    spain = region.region(get_region_key('spain'))
+    colombia = region.region(get_region_key('colombia'))
+    argentina = region.region(get_region_key('argentina'))
+    peru = region.region(get_region_key('peru'))
+    mexico = region.region(get_region_key('mexico'))
+    france = region.region(get_region_key('france'))
+
+    # returns list of region objects
+    countries = [
+        usa,
+        india,
+        brazil,
+        russia,
+        spain,
+        colombia,
+        argentina,
+        peru,
+        mexico,
+        france]
+
+    return countries
+
+# creates series of continent data, combines them into dataframe, and prints it
+def print_continents(continents):
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', None)
+
+    ### pandas series ###
+    # region names
+    s = [continents[i].get_region_name() for i in range(len(continents))]
+    names = pd.Series(s)
+
+    # total cases
+    s = [continents[i].get_total_cases() for i in range(len(continents))]
+    total_cases = pd.Series(s).astype('int32')
+
+    # new cases
+    s = [continents[i].get_new_cases() for i in range(len(continents))]
+    new_cases = pd.Series(s).astype('int32')
+
+    # total deaths
+    s = [continents[i].get_total_deaths() for i in range(len(continents))]
+    total_deaths = pd.Series(s).astype('int32')
+
+    # new deaths
+    s = [continents[i].get_new_deaths() for i in range(len(continents))]
+    new_deaths = pd.Series(s).astype('int32')
+
+    # mortality rate
+    s = [str(continents[i].get_mortality_rate()) + '%' for i in range(len(continents))]
+    mortality_rate = pd.Series(s).astype('str')
+
+    # constructing the dataframe
+    data = {
+        'continent:': names,
+        'total CASES' : total_cases,
+        'new CASES': new_cases,
+        'total DEATHS' : total_deaths,
+        'new DEATHS': new_deaths,
+        'mortality rate': mortality_rate,
+    }
+    df_continents = pd.DataFrame(data, columns=['continent:', 'total CASES', 'new CASES', 'total DEATHS',   'new DEATHS', 'mortality rate'])
+    print(df_continents)
+    print('\nFROM: ' + str(datetime.date.today()))
+
+# creates series of continent data, combines them into dataframe, and prints it
+def print_countries(countries):
+    ### pandas series ###
+    # region names
+    s = [countries[i].get_region_name() for i in range(len(countries))]
+    names = pd.Series(s)
+
+    # total cases
+    s = [countries[i].get_total_cases() for i in range(len(countries))]
+    total_cases = pd.Series(s).astype('int32')
+
+    # new cases
+    s = [countries[i].get_new_cases() for i in range(len(countries))]
+    new_cases = pd.Series(s)
+
+    # total deaths
+    s = [countries[i].get_total_deaths() for i in range(len(countries))]
+    total_deaths = pd.Series(s).astype('int32')
+
+    # new deaths
+    s = [countries[i].get_new_deaths() for i in range(len(countries))]
+    new_deaths = pd.Series(s)
+
+    # mortality rate
+    s = [str(countries[i].get_mortality_rate()) + '%' for i in range(len(countries))]
+    mortality_rate = pd.Series(s).astype('str')
+
+    # constructing the dataframe
+    data = {
+        'country:': names,
+        'total CASES' : total_cases,
+        'new CASES': new_cases,
+        'total DEATHS' : total_deaths,
+        'new DEATHS': new_deaths,
+        'mortality rate': mortality_rate,
+    }
+    df_countries = pd.DataFrame(data, columns=['country:', 'total CASES', 'new CASES', 'total DEATHS',   'new DEATHS', 'mortality rate'])
+    print(df_countries)
+    print('\nFROM: ' + str(datetime.date.today()))
+
+# run this to send the daily new cases and new deaths to an email address or phone number
+def daily_report():
+    # emailing or texting the daily report! #
+    phone_number = '{phone}'
+    email = '{email}'
+    country_names = [initialize_top10_countries()[i].get_region_name() for i in range(10)]
+    new_cases = [initialize_top10_countries()[i].get_new_cases() for i in range(10)]
+    new_deaths = [initialize_top10_countries()[i].get_new_deaths() for i in range(10)]
+
+    cases_message = str(datetime.date.today())
+    for i in range(10):
+        cases_message += '\n' + country_names[i] + ': ' + new_cases[i]
+    email_text_alerts.alert.email_alert(phone_number, 'New coronavirus cases today', cases_message)
+
+    deaths_message = str(datetime.date.today())
+    for i in range(10):
+        deaths_message += '\n' + country_names[i] + ': ' + new_deaths[i]
+    email_text_alerts.alert.email_alert(phone_number, 'New coronavirus deaths today', deaths_message)
+
 
 if __name__ == '__main__':
     main()
